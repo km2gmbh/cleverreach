dcefca09f93328fb24dd1e2ac83f0615e8c23095
David Brauer <david.brauer@km2.de>
2023-08-30 13:46:01 +0200

    [TASK] make installable in TYPO3 12.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,8 +13,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '11.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
-            'php' => '7.4.0-8.1.99',
+            'typo3' => '11.5.0-12.4.99',
+            'php' => '7.4.0-8.2.99',
         ],
         'conflicts' => [],
         'suggests' => [],
